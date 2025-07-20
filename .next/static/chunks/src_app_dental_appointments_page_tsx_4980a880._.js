9de6dd5f271eb02fe42520197f832e6d89fb2700
drzlm <86509636+drzlm@users.noreply.github.com>
2025-07-20 21:41:23 +0630

    Add .next/static/chunks/src_app_dental_appointments_page_tsx_4980a880._.js 1753024282824

---
+++ .next/static/chunks/src_app_dental_appointments_page_tsx_4980a880._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_86385586._.js",
+  "static/chunks/node_modules_283c462c._.js"
+],
+    source: "dynamic"
+});
